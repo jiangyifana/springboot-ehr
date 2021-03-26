a4c25482f934da294407935d55a9bfe1f40a9362
jiangyifan <1255220442@qq.com>
2021-03-26 20:07:32 +0800

    03-26 添加腾讯云环境

---
+++ src/main/java/cn/timelost/hr/MyHandler.java
@@ -0,0 +1,15 @@
+package cn.timelost.hr;
+
+import com.qcloud.scf.runtime.AbstractSpringHandler;
+
+/**
+ * @author: Jyf
+ * @Date: 2021/3/26 20:01
+ */
+public class MyHandler extends AbstractSpringHandler {
+    @Override
+    public void startApp() {
+        // 修改为项目的入口主函数
+        Application.main(new String[]{""});
+    }
+}
