f09fba7ab9c5b740e2130c7a440950099a220579
江益帆 <jiangyifan@polyv.net>
2023-06-30 15:13:19 +0800

    增加校验

--- src/main/java/cn/timelost/hr/exception/BaseException.java
+++ src/main/java/cn/timelost/hr/exception/BaseException.java
@@ -1,7 +1,6 @@
 package cn.timelost.hr.exception;
 
 import cn.timelost.hr.enums.ResultEnum;
-import lombok.Data;
 import lombok.Getter;
 
 /**
@@ -10,21 +9,28 @@ import lombok.Getter;
  */
 @Getter
 public class BaseException extends RuntimeException {
-
+    
     private static final long serialVersionUID = -4088898423313593435L;
     private String msg;
     private ResultEnum resultEnum;
-
+    
     public BaseException() {
     }
-
+    
     public BaseException(String msg) {
         super(msg);
         this.msg = msg;
     }
-
+    
     public BaseException(ResultEnum resultEnum) {
         super(resultEnum.getMessage());
         this.resultEnum = resultEnum;
     }
+    
+    public BaseException(ResultEnum resultEnum, Object... descArgs) {
+        super(String.format(resultEnum.getMessage(), descArgs));
+        this.msg = String.format(resultEnum.getMessage(), descArgs);
+        this.resultEnum = resultEnum;
+    }
+    
 }
